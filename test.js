4a5afb57d27a66fa94faad04aae063a1dc86857e
ahmadjumadi <ahmadjumadi@smkn1karawang.sch.id>
2023-02-01 10:22:55 +0700

    add feat:split, push, pop, and unshift for array from input

---
+++ test.js
@@ -0,0 +1,6 @@
+var numbers = [0, 1, 2]
+numbers.push(3)
+console.log(numbers) // [0, 1, 2, 3]
+// Bisa juga memasukkan lebih dari satu nilai menggunakan metode push
+numbers.push(4, 5)
+console.log(numbers) // [0, 1, 2, 3, 4, 5] 
